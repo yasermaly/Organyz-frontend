20bb1ee3b3950b8c2ea25f12bc440ee0fdf97ab2
Yaser Aly <77906579+yasermaly@users.noreply.github.com>
2022-06-16 18:45:19 -0700

    adds some styling to body and header

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/globalStyles.css'
+
+function Header({children}) {
+  return (
+    <h1 className='header'>
+        Organyz
+    </h1>
+  )
+}
+
+export default Header

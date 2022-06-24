211f76093865ed1cf3c8cbcdfdb5a6a6a2b66a34
h1n3s1ght <benjaminisaachines@gmail.com>
2022-06-24 08:01:50 -0500

    Welcome page styled

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,13 +1,14 @@
-import React from 'react'
+import React from 'react';
 import PageTitle from '../components/PageTitle';
-import { Link} from "react-router-dom"
+import { Link} from "react-router-dom";
+import style from "../styles/modules/welcome.module.scss"
 
 function Welcome({List, getList}) {
 
 const newUserId = List[List.length-1]._id;
  
   return (
-    <div>
+    <div className={style.welcomeDiv}>
         <PageTitle />
         <h1>Welcome {List[List.length-1].name}</h1>
         <h4>We are so glad you are here.  Let's get start!</h4>
